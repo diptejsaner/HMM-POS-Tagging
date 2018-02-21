2578cee2188125cbdd223d8bc2e16e8585bc92f7
Diptej Saner <sanerdiptej@gmail.com>
2018-02-20 20:20:55 -0800

    Initial commit with no smoothing implementation

---
+++ hmmdecode3.py
@@ -0,0 +1,88 @@
+import json
+import sys
+from operator import itemgetter
+
+
+def get_pos_tags_viterbi(line):
+    line = line.lower()
+    words = line.split(' ')
+    pos_tags = []
+
+    probs = [[0 for i in range(len(words))] for j in range(len(tg) + 1)]
+    backpointer = [[0 for i in range(len(words))] for j in range(len(tg))]
+
+    states = [s for s in tg]
+
+    words[-1] = words[-1].rstrip()
+    sindex = 0
+    for state in states:
+        if words[0] in eg[state] and state in tg["q0"]:
+            probs[sindex][0] = tg["q0"][state] * eg[state][words[0]]
+        sindex += 1
+
+    for windex in range(1, len(words)):
+        word = words[windex]
+        for sindex in range(0, len(states)):
+            plist = []
+            state = states[sindex]
+            # loop for calculating the max
+            for ind in range(0, len(states)):
+                prevstate = states[ind]
+                if state in tg[prevstate] and word in eg[state]:
+                    p = probs[ind][windex - 1] * eg[state][word] * tg[prevstate][state]
+                    plist.append([ind, p])
+                else:
+                    plist.append([ind, 0])
+
+            smax, pmax = max(plist, key=itemgetter(1))
+            probs[sindex][windex] = pmax
+            backpointer[sindex][windex] = smax
+
+    maxp = probs[0][-1]
+    ind = -1
+    for sindex in range(0, len(states)):
+        if maxp < probs[sindex][-1]:
+            maxp = probs[sindex][-1]
+            ind = sindex
+
+    pos_tags.append(states[ind])
+    for col in range(len(words) - 1, 0, -1):
+        pos_tags.append(states[backpointer[ind][col]])
+        ind = backpointer[ind][col]
+
+    pos_tags.reverse()
+    return pos_tags
+
+
+def tag_sentence(line, pos_tags):
+    words = line.split(' ')
+    tagged_words = []
+
+    for word, tag in list(zip(words, pos_tags)):
+        word = word.rstrip()
+        word += "/" + tag
+        tagged_words.append(word)
+
+    tagged_words[-1] += "\n"
+
+    return " ".join(tagged_words[:])
+
+
+tg = {}
+eg = {}
+
+with open('transitionMatrix.json', 'r', encoding='utf8') as f:
+    tg = json.load(f)
+
+with open('emissionMatrix.json', 'r', encoding='utf8') as f:
+    eg = json.load(f)
+
+outputFile = open("hmmoutput.txt", "w", encoding='utf8')
+
+with open(sys.argv[1]) as f:
+    pos_tags = []
+    for line in f:
+        pos_tags = get_pos_tags_viterbi(line)
+        outputFile.write(tag_sentence(line, pos_tags))
+
+outputFile.close()
---
+++ hmmlearn3.py
@@ -0,0 +1,124 @@
+import json
+import sys
+
+f = open(sys.argv[1], "r", encoding='utf8')
+
+tg = {}
+eg = {}
+
+line_count = 0
+word_count = 0
+
+first_tag_prob = {}
+
+for line in f:
+    words = line.split(' ')
+    k = 0
+    prev = ""
+
+    for word in words:
+        x = word.split('/')
+
+        x[0:-1] = ["/".join(x[0:-1])]
+        w = x[0].lower()
+        #w = x[0]
+
+        tag = x[-1].rstrip()
+
+        if k == 0:
+            if tag not in first_tag_prob:
+                first_tag_prob[tag] = 1
+            else:
+                first_tag_prob[tag] += 1
+
+        if prev != "":
+            if tag in tg[prev]:
+                tg[prev][tag] += 1
+            else:
+                tg[prev][tag] = 1
+
+        if tag not in tg:
+            tg[tag] = {}
+
+        if tag not in eg:
+            eg[tag] = {}
+            eg[tag][w] = 1
+        elif w not in eg[tag]:
+            eg[tag][w] = 1
+        else:
+            eg[tag][w] += 1
+
+        prev = tag
+        word_count += 1
+        k += 1
+    line_count += 1
+
+eg["q0"] = {}
+tg["q0"] = {}
+for node in first_tag_prob:
+    first_tag_prob[node] /= line_count
+    tg["q0"][node] = first_tag_prob[node]
+
+f.close()
+
+# convert to probabilities
+for tag in tg:
+    count = 0
+
+    for t in tg[tag]:
+        count += tg[tag][t]
+
+    for t in tg[tag]:
+        tg[tag][t] /= count
+
+for tag in eg:
+    count = 0
+
+    for wd in eg[tag]:
+        count += eg[tag][wd]
+
+    for wd in eg[tag]:
+        eg[tag][wd] /= count
+
+#print("\nNumber of lines : " + str(line_count))
+#print("\nnumber of tags found : " + str(len(tg)))
+
+with open('transitionMatrix.json', 'w', encoding='utf8') as fp:
+    json.dump(tg, fp, ensure_ascii=False)
+
+with open('emissionMatrix.json', 'w', encoding='utf8') as fp:
+    json.dump(eg, fp, ensure_ascii=False)
+
+'''with open("/Users/diptejsaner/Desktop/NLP_HMM/transitionMatrix.txt", "w") as file:
+	for node in tg:
+		tags.append(node)
+		file.write("%s " % node)
+
+	file.write("\n")
+
+	for node in tags:
+		for adjNode in tags:
+			if adjNode in tg[node]:
+				file.write("%f " % tg[node][adjNode])
+			else:
+				file.write("%i " % 0)
+		file.write("\n")
+
+with open("/Users/diptejsaner/Desktop/NLP_HMM/emissionMatrix.txt", "w") as file:
+	for tag in tags:
+		file.write("%s " % tag)
+	file.write("\n")
+
+	for word in wordset:
+		file.write("%s " % word)
+	file.write("\n")
+
+	ind = 0
+	for node in tags:
+		ind = 0
+		for eword in wordset:
+			if eword in eg[node]:
+				file.write("%i:%f " % (ind, eg[node][eword]))
+			ind += 1
+		file.write("\n")
+'''
